2d80d5c144d934ea7b1ff09942bcfb0f5ec10248
Ciscodefran <jellycisko@gmail.com>
2021-08-05 22:51:00 +0900

    first commit

---
+++ src/test/java/com/icia/wapoo/WapooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icia.wapoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WapooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
