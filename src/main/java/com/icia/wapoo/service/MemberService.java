6a2ccdf148055416d82de0d070b292fd0a2ac63c
Ciscodefran <jellycisko@gmail.com>
2021-08-06 10:05:35 +0900

    부트스트랩 추가 es6-promise 의존성 추가

--- src/main/java/com/icia/wapoo/service/MemberService.java
+++ src/main/java/com/icia/wapoo/service/MemberService.java
@@ -1,11 +1,10 @@
 package com.icia.wapoo.service;
 
-import com.icia.wapoo.dao.MemberDao;
 import com.icia.wapoo.model.MemberVO;
-import org.springframework.beans.factory.annotation.Autowired;
+
 
 import java.util.List;
 
 public interface MemberService {
-    public List<MemberVO> getMembers();
+    List<MemberVO> getMembers();
 }
