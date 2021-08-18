7700699f2187f5a8e8b0a354f4e4babc9ed53b62
wlsglckals@gmail.com <wlsglckals@gmail.com>
2021-08-19 08:52:07 +0900

    store detail

--- front/src/views/food/FoodDetail.vue
+++ front/src/views/food/FoodDetail.vue
@@ -19,41 +19,126 @@
     <td class="tg-b420">08:30 ~ 16:50</td>
   </tr>
   <tr>
-    <td class="tg-zv4m" colspan="2">우리 가게는 국내산만 취급합니다. <br>가족들이 먹는다는 생각으로 음식을 만듭니다.</td>
+    <td class="tg-b420"  colspan="3" >결제정보</td>
+  </tr>
+  <tr>
+    <td class="tg-b420">최소주문금액</td>
+    <td class="tg-b420">8,000원</td>
+  </tr>
+  <tr>
+    <td class="tg-b420">배달요금</td>
+    <td class="tg-b420">2,000원</td>
+  </tr>
+  <tr>
+    <td class="tg-b420">결제수단</td>
+    <td class="tg-b420">신용카드, 카카오페이, 현장결제</td>
+  </tr>
+  <tr>
+    <td class="tg-b420"  colspan="3" >사업자정보</td>
+  </tr>
+  <tr>
+    <td class="tg-b420">상호명</td>
+    <td class="tg-b420">홍콩반점</td>
+  </tr>
+  <tr>
+    <td class="tg-b420">사업자등록번호</td>
+    <td class="tg-b420">123-45-67890</td>
+  </tr>
+  <tr>
+    <td class="tg-b420"  colspan="3" >원산지정보</td>
+  </tr>
+  <tr>
+    <td class="tg-b420" colspan="3">
+      양파:국내산<br>
+      돼지고기:국내산<br>
+      소고기:미국산<br>
+      면:국내산<br>
+      마늘:국내산<br>
+      새우:베트남산<br>
+      고추가루:중국산<br>
+      쌀:국내산<br>
+      단무지:중국산<br>
+      피망:일본산<br>
+      배추:국내산<br>
+      굴:국내산<br>
+      닭고기:브라질산<br>
+      김치:중국산<br>
+    </td>
+  </tr>
+  <tr>
+    <td class="tg-zv4m" colspan="2">
+          우리 가게는 국내산만 취급합니다. 
+      <br>가족들이 먹는다는 생각으로 음식을 만듭니다.
+      <br>현장결제시 e음카드 사용가능합니다.</td>
   </tr>
 </tbody>
 </table>
-
---------------------------------메뉴-----------------------------------
 
 <table class="tg">
 <thead>
   <tr>
-    <th class="tg-kjb5" colspan="2">음식메뉴</th>
+    <th class="tg-08up" colspan="8">메뉴</th>
   </tr>
 </thead>
 <tbody>
   <tr>
-    <td class="tg-xs9c">짜장면</td>
-    <td class="tg-xs9c">4,500 원</td>
+    <td class="tg-9e2s" colspan="2">면류</td>
+    <td class="tg-9e2s" colspan="2">밥류</td>
+    <td class="tg-9e2s" colspan="2">요리류</td>
+    <td class="tg-9e2s" colspan="2">세트류</td>
   </tr>
   <tr>
-    <td class="tg-xs9c">짬뽕</td>
-    <td class="tg-xs9c">5,000 원</td>
+    <td class="tg-9e2s">짜장면</td>
+    <td class="tg-9e2s">4,500</td>
+    <td class="tg-9e2s">볶음밥</td>
+    <td class="tg-9e2s">7,000</td>
+    <td class="tg-9e2s">탕수육</td>
+    <td class="tg-9e2s">17,000</td>
+    <td class="tg-73sp">가 세트<br>(짜장+탕수육)</td>
+    <td class="tg-9e2s">17,000</td>
   </tr>
   <tr>
-    <td class="tg-xs9c">탕수육</td>
-    <td class="tg-xs9c">12,000 원</td>
+    <td class="tg-9e2s">짬뽕</td>
+    <td class="tg-9e2s">5,500</td>
+    <td class="tg-9e2s">짜장밥</td>
+    <td class="tg-9e2s">7,500</td>
+    <td class="tg-9e2s">깐풍기</td>
+    <td class="tg-9e2s">25,000</td>
+    <td class="tg-73sp">나세트<br>(짬뽕+탕수육)</td>
+    <td class="tg-9e2s">18,000</td>
   </tr>
   <tr>
-    <td class="tg-xs9c">세트메뉴1<br>(짜장+탕수육)</td>
-    <td class="tg-xs9c">15,000 원</td>
+    <td class="tg-9e2s">간짜장</td>
+    <td class="tg-9e2s">6,500</td>
+    <td class="tg-9e2s">잡채밥</td>
+    <td class="tg-9e2s">8,000</td>
+    <td class="tg-9e2s">양장피</td>
+    <td class="tg-9e2s">26,000</td>
+    <td class="tg-73sp">다세트<br>(짜장2+탕수육)</td>
+    <td class="tg-9e2s">19,000</td>
+  </tr>
+  <tr>
+    <td class="tg-9e2s">우동</td>
+    <td class="tg-9e2s">7,000</td>
+    <td class="tg-9e2s">짬뽕밥</td>
+    <td class="tg-9e2s">8,000</td>
+    <td class="tg-9e2s">팔보채</td>
+    <td class="tg-9e2s">26,000</td>
+    <td class="tg-73sp">라세트<br>(짬뽕2+탕수육)</td>
+    <td class="tg-9e2s">21,000</td>
+  </tr>
+  <tr>
+    <td class="tg-9e2s">울면</td>
+    <td class="tg-9e2s">7,000</td>
+    <td class="tg-9e2s">송이덮밥</td>
+    <td class="tg-9e2s">9,000</td>
+    <td class="tg-9e2s">유산슬</td>
+    <td class="tg-9e2s">30,000</td>
+    <td class="tg-73sp">마세트<br>(짜장+짬뽕+탕수육)</td>
+    <td class="tg-9e2s">25,000</td>
   </tr>
 </tbody>
 </table>
-
-
---------------------------------리뷰-----------------------------------
 
 
 <table class="tg">
@@ -77,79 +162,40 @@
     <td class="tg-8jgo">★★★★☆</td>
   </tr>
   <tr>
-    <td class="tg-zv4m">리뷰 0개</td>
-    <td class="tg-zv4m" colspan="2">사장님댓글 0개</td>
-  </tr>
-</tbody>
-</table>
-<ul id="review" class="list-group review-list">
-<li class="list-group-item ng-hide" ng-show="restaurant.reviews.length<1">
-<p class="review-empty clearfix ng-binding">주문 후 첫번째 리뷰를 남겨보세요.</p>
-</li>
-<li class="list-group-item star-point ng-scope" ng-repeat="review in restaurant.reviews" on-finish-render="scrollCartArea()">
-<div>
-<span ng-show="review.phone" class="review-id ng-binding">#{userId)</span>
-<span ng-bind="review.time|since" class="review-time ng-binding">#{sysdate}</span>
-<a href class="btn-report" ng-click="report_reply(review.id, 'user', review)">신고</a>
-</div>
-<div>
-<div class="star-point">
-<span class="total">
-<span class="full ng-scope" ng-repeat="i in review.rating|number_to_array track by $index">★</span>
-<span class="full ng-scope" ng-repeat="i in review.rating|number_to_array track by $index">★</span>
-<span class="full ng-scope" ng-repeat="i in review.rating|number_to_array track by $index">★</span>
-<span class="full ng-scope" ng-repeat="i in review.rating|number_to_array track by $index">★</span>
-<span class="full ng-scope" ng-repeat="i in review.rating|number_to_array track by $index">★</span>
-</span>
-<span class="category" ng-show="review.rating_taste > 0 || review.rating_quantity > 0 || review.rating_delivery > 0">
-                <span class="category-tit" ng-show="review.rating_taste > 0">맛</span>
-                <span class="full" ng-show="review.rating_taste > 0">★</span>
-                <span class="points ng-binding" ng-show="review.rating_taste > 0">5</span>
-                <span class="category-tit" ng-show="review.rating_quantity > 0">양</span>
-                <span class="full" ng-show="review.rating_quantity > 0">★</span>
-                <span class="points ng-binding" ng-show="review.rating_quantity > 0">5</span>
-                <span class="category-tit" ng-show="review.rating_delivery > 0">배달</span>
-                <span class="full" ng-show="review.rating_delivery > 0">★</span>
-                <span class="points ng-binding" ng-show="review.rating_delivery > 0">5</span>
-              </span>
-</div>
-</div>
-<div class="order-items default ng-binding" ng-click="show_review_menu($event)">
-            #{시킨음식}<!-- 짜장면1, 짬뽕1 -->
-          </div>
-<p ng-show="review.comment" ng-bind-html="review.comment|strip_html" class="ng-binding">#{작성글}<!-- 맛있게 잘먹었어요~ --></p>
-</li>
-</ul>
-
-------------------------------밑엔 표로 만든 리뷰---------------------------------------------
-
-<table class="tg">
-<thead>
-  <tr>
-    <th class="tg-4z9b">#{유저아이디}</th>
-    <th class="tg-4z9b">#{작성시간}</th>
-    <th class="tg-4z9b">신고</th>
-  </tr>
-</thead>
-<tbody>
-  <tr>
-    <td class="tg-4z9b">★★★★★</td>
-    <td class="tg-4z9b" colspan="2">맛5 양5 배달5</td>
+    <td class="tg-8jgo" colspan="3">리뷰 0개 / 사장님댓글 0개</td>
   </tr>
   <tr>
-    <td class="tg-4z9b" colspan="3">#{시킨음식메뉴} </td>
+     <td class="tg-8jgo">UserId</td>
+     <td class="tg-8jgo">sysdate</td>
+     <td>
+			<input type="button" id="Declaration" class="btn btn-outline-primary" value="신고하기"  style="float:right">		</td>
   </tr>
   <tr>
-    <td class="tg-4z9b" colspan="3">#{작성글내용}</td>
+    <td class="tg-8jgo">★★★★★</td>
+    <td class="tg-8jgo" colspan="2">맛★5 양★5 배달★5</td>
   </tr>
+  <tr>
+    <td class="tg-8jgo" colspan="3">시킨음식메뉴</td>
+  </tr>
+  <tr>
+    <td class="form-group" colspan="3">
+   <textarea class="form-control" rows="6" id="content"
+    name="content" placeholder="주문 후 리뷰작성해주세요."></textarea>
+ 
+ <form name="첨부파일" action="review_photo_file" method="post" enctype="multipart/form-data">
+        <input type='file' name='' />
+ <button type="button" class="btn btn-default">등록</button>
+ </form>
+  </td>
+ </tr>
 </tbody>
 </table>
 </template>
 
-<style scoped>
+<style type="text/css">
     table
 {
-	position: relative;
+   position: relative;
     border: 1px solid #555555;
     background: #fff;
     padding: 0 8px;
@@ -159,7 +205,7 @@
 }
 ul
 {
-	list-style:none;
+   list-style:none;
 }
 
 .tg  {border-collapse:collapse;border-spacing:0;}
@@ -170,30 +216,28 @@ ul
 .tg .tg-zv4m{border-color:#ffffff;text-align:left;vertical-align:top}
 .tg .tg-9fjy{border-color:#ffffff;font-family:Impact, Charcoal, sans-serif !important;;font-size:24px;font-weight:bold;
   text-align:center;vertical-align:top}
-.tg .tg-b420{border-color:#ffffff;font-size:18px;text-align:center;vertical-align:top}
+.tg .tg-b420{border-color:#ffffff;font-size:15px;text-align:center;vertical-align:top}
 
 .tg  {border-collapse:collapse;border-spacing:0;}
 .tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
-  overflow:hidden;padding:10px 5px;word-break:normal;}
+  overflow:hidden;padding:0px 1px;word-break:normal;}
 .tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
-  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
-.tg .tg-kjb5{border-color:#efefef;font-size:24px;font-weight:bold;text-align:center;vertical-align:top}
-.tg .tg-xs9c{border-color:#efefef;font-size:13px;text-align:center;vertical-align:top}
+  font-weight:normal;overflow:hidden;padding:0px 1px;word-break:normal;}
+.tg .tg-73sp{background-color:#ffffff;border-color:#ffffff;color:#040000;
+  font-family:"Comic Sans MS", cursive, sans-serif !important;;font-size:10px;text-align:center;vertical-align:top}
+.tg .tg-08up{background-color:#ffffff;border-color:#ffffff;color:#040000;
+  font-family:"Comic Sans MS", cursive, sans-serif !important;;font-size:15px;font-weight:bold;text-align:center;
+  vertical-align:top}
+.tg .tg-9e2s{background-color:#ffffff;border-color:#ffffff;color:#040000;
+  font-family:"Comic Sans MS", cursive, sans-serif !important;;font-size:13px;text-align:center;vertical-align:top}
 
 .tg  {border-collapse:collapse;border-spacing:0;}
-.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:13px;
   overflow:hidden;padding:10px 5px;word-break:normal;}
-.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:13px;
   font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
 .tg .tg-zv4m{border-color:#ffffff;text-align:left;vertical-align:top}
-.tg .tg-8jgo{border-color:#ffffff;text-align:center;vertical-align:top}
-.tg .tg-0q9k{border-color:#ffffff;font-size:28px;font-weight:bold;text-align:center;vertical-align:top}
-.tg .tg-v0mg{border-color:#ffffff;text-align:center;vertical-align:middle}
-
-.tg  {border-collapse:collapse;border-spacing:0;}
-.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
-  overflow:hidden;padding:10px 5px;word-break:normal;}
-.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
-  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
-.tg .tg-4z9b{border-color:#efefef;font-size:16px;text-align:left;vertical-align:top}
+.tg .tg-8jgo{border-color:#535050;text-align:center;vertical-align:top}
+.tg .tg-0q9k{border-color:#413f3f;font-size:28px;font-weight:bold;text-align:center;vertical-align:top}
+.tg .tg-v0mg{border-color:#525050;text-align:center;vertical-align:middle;}
 </style>
