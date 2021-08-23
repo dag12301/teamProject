7d3725df184b7706a93aea43a5a93500e5fb6b38
Ciscodefran <jellycisko@gmail.com>
2021-08-23 09:39:42 +0900

    풀전

--- src/main/java/com/icia/wapoo/dao/ArticleDao.java
+++ src/main/java/com/icia/wapoo/dao/ArticleDao.java
@@ -15,20 +15,20 @@ public interface ArticleDao {
 
 	List<Article> selectAllByBoardId(@Param("boardId") long boardId);
 	//게시글 등록
-	public int boardInsert(Article article);
+	int boardInsert(Article article);
 	// 게시글 조회
-	public Article boardSelect(long articleId);
+	Article boardSelect(long articleId);
 	//게시글 삭제
-	public long boardDelete(long articleId);
+	long boardDelete(long articleId);
 	//게시글 view
-	public Article boardList(@Param("articleId") long articleId);
+	Article boardList(@Param("articleId") long articleId);
 	//조회수 증가
-	public int boardHit(@Param("articleId") long articleId);
+	int boardHit(@Param("articleId") long articleId);
 	
 	//총 게시글 개수 확인
-	public int getBoardListCnt(long boardId) throws Exception;
+	int getBoardListCnt(long boardId) throws Exception;
 	// 페이징 개수
-	public List<Article> getBoardList(PagingA paging, long boardId) throws Exception;
+	List<Article> getBoardList(PagingA paging, long boardId) throws Exception;
 
 	
 
