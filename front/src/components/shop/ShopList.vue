b58a62d2ddbe06881d23de437404f147fca23dec
dag12301 <dag1230@naver.com>
2021-08-26 08:46:04 +0900

    커밋

--- front/src/components/shop/ShopList.vue
+++ front/src/components/shop/ShopList.vue
@@ -1,39 +1,30 @@
 <template>
-  <div class="shopList">
-    <div class="shopBox">
-      <a
-        href="#"
-        class="
-          list-group-item list-group-item-action
-          flex-column
-          align-items-start
-        "
-      >
-        <div class="row">
-          <div class="col storeTitle">
-            <h5 class="mb-1">{{ shopInfo.name }}</h5>
-          </div>
-          <!-- 음식사진시작 -->
-          <div>
-            <div class="slide">
-              <splide :options="options">
-                <splide-slide v-for="file in fileList" :key="file">
-                  <img class="menuImg" :src="file.name" />
-                </splide-slide>
-              </splide>
-            </div>
-          </div>
-          <!-- 종료 -->
-          <div class="col-1 storeKind">
-            <small class="text-muted">{{ shopInfo.storeKind }}</small>
-          </div>
-        </div>
-        <div class="row address">
-          <small> {{ shopInfo.address }}, {{ shopInfo.addressDetail }} </small>
-        </div>
-      </a>
+  <a href="#" class="shopList">
+    <div class="row storeHeader">
+      <div class="col-10 storeTitle">
+        <h5 class="mb-1">{{ shopInfo.name }}</h5>
+      </div>
+      <div class="col-2 storeKind">
+        <small class="text-muted">{{ shopInfo.storeKind }}</small>
+      </div>
     </div>
-  </div>
+    <div class="row">
+      <!-- 음식사진시작 -->
+      <div class="col-12">
+        <div class="slide">
+          <splide :options="options">
+            <splide-slide v-for="file in fileList" :key="file">
+              <img class="menuImg" :src="file.name" />
+            </splide-slide>
+          </splide>
+        </div>
+      </div>
+      <!-- 종료 -->
+    </div>
+    <div class="row address">
+      <small> {{ shopInfo.address }}, {{ shopInfo.addressDetail }} </small>
+    </div>
+  </a>
 </template>
 
 <script>
@@ -75,32 +66,29 @@ export default {
       fileList: [],
       options: {
         rewind: true, //첫 번째 슬라이드 이전 또는 마지막 슬라이드 이후에 슬라이더를 되감는지 여부.
-        gap: "2em", // 슬라이드 사이의 간격.
         speed: 300, // 밀리초 단위의 전환 속도입니다.
         rewindSpeed: 300, //되감기 시 전환 속도(밀리초)입니다.
-        perPage: 5, //페이지당 표시할 슬라이드 수를 결정합니다.
-        perMove: 1, //슬라이더가 다음 또는 이전 페이지로 이동할 때 이동해야 하는 슬라이드 수를 결정합니다.
-        autoplay: true, //자동재생 활성화 여부.
-        interval: 3000, //밀리초 단위의 자동 재생 간격.
-        pauseOnHover: true, //슬라이더를 가리키고 있는 동안 자동 재생을 중지할지 여부입니다.
-        resetProgress: true, //재개 시 자동 재생 타이머의 진행 상황을 재설정할지 여부입니다.
         focus: "center", //어떤 슬라이드에 초점을 맞춰야 하는지 결정합니다.
         type: "loop",
         breakpoints: {
+          1600 : {
+            width: 700,
+            perPage: 0,
+          },
           1200 : {
-            width: 600,
+            width: 700,
             perPage: 5,
           },
           1100 : {
             width: 500,
             perPage: 3,
           },
-          1000 : {
+          900 : {
             width: 400,
             perPage: 3,
           },
-          900 : {
-           width: 300,
+          700 : {
+            width: 300,
             perPage: 3,
           },
         },
@@ -113,23 +101,17 @@ export default {
 <style scoped>
 .shopList {
   width: 100%;
-  height: 500px;
   display: grid;
-}
-.shopBox {
-  height: 100px;
-}
-.menuImg {
-  width: 100px;
-  height: 70px;
+  justify-content: space-around;
+  color:black; 
+  text-decoration: none;
 }
 .storeTitle {
   text-align: left;
-  padding: 0px;
+  padding: 5px 0 0;
 }
 .storeKind {
-  width: 70px;
-  padding: 0px;
+  padding: 5px 0 0;
 }
 .address {
   text-align: left;
@@ -138,15 +120,24 @@ export default {
 }
 @media screen and (min-width: 1200px) {
   .slide {
-    position: absolute;
+    width: 300px;
     display: none;
-    width: 100px;
+  }
+  .menuImg {
+    width: 0px;
+    height: 0px;
+  }
+  .storeHeader {
+    width: 300px;
+    height: 50px;
   }
 }
 @media screen and (max-width: 1200px) {
-  .slide {
-    position: absolute;
-    width: 700px;
+  .menuImg {
+  width: 100px;
+  height: 70px;
   }
 }
 </style>
+  
+  
